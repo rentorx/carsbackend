b4aa97a717db043ff7c1bf815c1b40b0c17f6272
rentorx <ingrenetorres@gmail.com>
2019-06-13 00:40:39 -0500

    adding model fields

--- companies/models.py
+++ companies/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from commons.models import TimeStampedModel
 from django.utils.translation import gettext_lazy as _
 from autoslug import AutoSlugField
+#from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Company(TimeStampedModel):
@@ -9,7 +10,18 @@ class Company(TimeStampedModel):
     labeled companies
     """
 
-    name = models.CharField(_('name'), max_length=255)
+    name = models.CharField(_('name'), max_length=128)
+    address = models.CharField(_('address'), max_length=128)
+    phone = models.CharField(_('phone'), max_length=128)
+#    phone = PhoneNumberField(_('phone'), null=False, blank=False, unique=True)
+    email = models.EmailField(
+        _('email'),
+        max_length=70,
+        null=True,
+        blank=True,
+        unique=True,
+    )
+
     slug = AutoSlugField(
         populate_from='name',
         unique=True,
